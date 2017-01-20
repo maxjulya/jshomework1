da2cc9b2bc1f0a9fe0697eadf8b6819ad9d3a652
maxjulya <maxjulya@gmail.com>
2017-01-20 10:22:02 +0200

    commit2

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 
 
-var arr=["Юра","Саша","Маша","Даша","Степаша" ];
- 
-var Word=prompt("Введите имя"); 
- 
-for (var i=0; i < arr.length; ++i) 
-	if (arr[i]==Word) 
-	{ alert( Word + ", вы успешно вошли"); break; } 
-	if (i==arr.length) alert("Имя не найдено");
+function pow(x, n) {
+	var result = 1;
+	for (var i = 0; i < n; i++) {
+		result *= x;
+	}
+
+	return result;
+}
+
+var x = prompt('Укажите число');
+var n = prompt('Укажите стерень');
+
+if (n < 0) {
+	alert('Стерень ' + n + ' не поддерживается, введите целую стерень, большую 0');
+}
+	else {
+	alert( pow(x, n) );
+	}
 
 
